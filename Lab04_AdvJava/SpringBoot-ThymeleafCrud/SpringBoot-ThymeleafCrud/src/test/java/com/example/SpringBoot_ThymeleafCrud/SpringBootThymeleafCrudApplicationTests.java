0e08418be633d69e5cd766f1abfe78d8d7609084
Hồ Du Tuấn Đạt <tdat.100524@gmail.com>
2025-10-02 20:01:52 +0700

    Add files via upload

---
+++ Lab04_AdvJava/SpringBoot-ThymeleafCrud/SpringBoot-ThymeleafCrud/src/test/java/com/example/SpringBoot_ThymeleafCrud/SpringBootThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_ThymeleafCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
